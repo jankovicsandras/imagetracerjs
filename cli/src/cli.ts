39d8e7bbc1a5981014d855656b60317055bab92a
Sebastián Gurín <sgurin@awalabs.com>
2019-06-18 10:20:58 -0300

    lint

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -9,21 +9,17 @@ var ImageTracer = require('imagetracerjs');
 export async function traceImage(options: Options) {
   preconditions(options)
   options.debug && console.log(`CLI Options: ${JSON.stringify({ ...options, input: null })}`)
-
   const input = (typeof options.input === 'string' ? glob(options.input).filter(existsSync) : [])
     .map(f => ({
       name: f,
       content: readFileSync(f)
     }))
-
   if (!input.length) {
     fail(`No input files found for ${input}. Aborting. `)
   }
-
   if (options.output && !existsSync(options.output)) {
     mkdirSync(options.output, { recursive: true })
   }
-
   await serial(input.map(input => async () => {
     try {
       options.debug && console.log('Rendering ' + input.name)
@@ -90,52 +86,3 @@ Options:
 * --blurdelta: number: RGBA delta treshold for selective Gaussian blur preprocessing. Default value: 20.
   `)
 }
-
-
-
-// var infilename = process.argv[2], outfilename = infilename+'.svg', options = {}, thisargname = '';
-// if(process.argv.length>3){
-
-// 	for(var i=3; i<process.argv.length; i+=2 ){
-
-// 		thisargname = process.argv[i].toLowerCase();
-
-// 		// Output file name
-// 		if(thisargname === 'outfilename' || thisargname === '-outfilename'){ outfilename = process.argv[i+1]; }
-
-// 		// Tracing
-// 		if(thisargname === 'corsenabled' || thisargname === '-corsenabled'){ options.corsenabled = (process.argv[i+1].toLowerCase() === 'true'); }
-// 		if(thisargname === 'ltres' || thisargname === '-ltres'){ options.ltres = parseFloat(process.argv[i+1]); }
-// 		if(thisargname === 'qtres' || thisargname === '-qtres'){ options.qtres = parseFloat(process.argv[i+1]); }
-// 		if(thisargname === 'pathomit' || thisargname === '-pathomit'){ options.pathomit = parseInt(process.argv[i+1]); }
-// 		if(thisargname === 'rightangleenhance' || thisargname === '-rightangleenhance'){ options.rightangleenhance = (process.argv[i+1].toLowerCase() === 'true'); }
-
-// 		// Color quantization
-// 		if(thisargname === 'colorsampling' || thisargname === '-colorsampling'){ options.colorsampling = parseInt(process.argv[i+1]); }
-// 		if(thisargname === 'numberofcolors' || thisargname === '-numberofcolors'){ options.numberofcolors = parseInt(process.argv[i+1]); }
-// 		if(thisargname === 'mincolorratio' || thisargname === '-mincolorratio'){ options.mincolorratio = parseFloat(process.argv[i+1]); }
-// 		if(thisargname === 'colorquantcycles' || thisargname === '-colorquantcycles'){ options.colorquantcycles = parseInt(process.argv[i+1]); }
-
-// 		// Layering method
-// 		if(thisargname === 'layering' || thisargname === '-layering'){ options.layering = process.argv[i+1]; }
-
-// 		// SVG rendering
-// 		if(thisargname === 'strokewidth' || thisargname === '-strokewidth'){ options.strokewidth = parseFloat(process.argv[i+1]); }
-// 		if(thisargname === 'linefilter' || thisargname === '-linefilter'){ options.linefilter = (process.argv[i+1].toLowerCase() === 'true'); }
-// 		if(thisargname === 'scale' || thisargname === '-scale'){ options.scale = parseFloat(process.argv[i+1]); }
-// 		if(thisargname === 'roundcoords' || thisargname === '-roundcoords'){ options.roundcoords = parseInt(process.argv[i+1]); }
-// 		if(thisargname === 'viewbox' || thisargname === '-viewbox'){ options.viewbox = (process.argv[i+1].toLowerCase() === 'true'); }
-// 		if(thisargname === 'desc' || thisargname === '-desc'){ options.desc = (process.argv[i+1].toLowerCase() === 'true'); }
-// 		if(thisargname === 'lcpr' || thisargname === '-lcpr'){ options.lcpr = parseFloat(process.argv[i+1]); }
-// 		if(thisargname === 'qcpr' || thisargname === '-qcpr'){ options.qcpr = parseFloat(process.argv[i+1]); }
-
-// 		// Blur
-// 		if(thisargname === 'blurradius' || thisargname === '-blurradius'){ options.blurradius = parseInt(process.argv[i+1]); }
-//     if(thisargname === 'blurdelta' || thisargname === '-blurdelta'){ options.blurdelta = parseInt(process.argv[i+1]); }		
-
-//     if()
-
-// 	}// End of argv loop
-
-// }// End of command line argument list length check
-
