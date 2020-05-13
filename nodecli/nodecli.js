fd0d98a6f985ac29b76e8b9168bbe2c690a8722d
Ondrej Brinkel <info@anzui.de>
2020-05-13 13:39:34 +0200

    handles relative and absolute paths correctly #42

--- nodecli/nodecli.js
+++ nodecli/nodecli.js
@@ -4,21 +4,21 @@ var fs = require('fs');
 
 var ImageTracer = require( __dirname + '/../imagetracer_v1.2.5' );
 
-// This example uses https://github.com/arian/pngjs 
+// This example uses https://github.com/arian/pngjs
 // , but other libraries can be used to load an image file to an ImageData object.
 var PNGReader = require( __dirname + '/PNGReader' );
 
 // CLI arguments to options
 var infilename = process.argv[2], outfilename = infilename+'.svg', options = {}, thisargname = '';
 if(process.argv.length>3){
-	
+
 	for(var i=3; i<process.argv.length; i+=2 ){
-		
+
 		thisargname = process.argv[i].toLowerCase();
-		
+
 		// Output file name
 		if(thisargname === 'outfilename' || thisargname === '-outfilename'){ outfilename = process.argv[i+1]; }
-		
+
 		// Tracing
 		if(thisargname === 'corsenabled' || thisargname === '-corsenabled'){ options.corsenabled = (process.argv[i+1].toLowerCase() === 'true'); }
 		if(thisargname === 'ltres' || thisargname === '-ltres'){ options.ltres = parseFloat(process.argv[i+1]); }
@@ -34,7 +34,7 @@ if(process.argv.length>3){
 
 		// Layering method
 		if(thisargname === 'layering' || thisargname === '-layering'){ options.layering = process.argv[i+1]; }
-		
+
 		// SVG rendering
 		if(thisargname === 'strokewidth' || thisargname === '-strokewidth'){ options.strokewidth = parseFloat(process.argv[i+1]); }
 		if(thisargname === 'linefilter' || thisargname === '-linefilter'){ options.linefilter = (process.argv[i+1].toLowerCase() === 'true'); }
@@ -47,39 +47,39 @@ if(process.argv.length>3){
 
 		// Blur
 		if(thisargname === 'blurradius' || thisargname === '-blurradius'){ options.blurradius = parseInt(process.argv[i+1]); }
-		if(thisargname === 'blurdelta' || thisargname === '-blurdelta'){ options.blurdelta = parseInt(process.argv[i+1]); }		
-		
+		if(thisargname === 'blurdelta' || thisargname === '-blurdelta'){ options.blurdelta = parseInt(process.argv[i+1]); }
+
 	}// End of argv loop
-	
+
 }// End of command line argument list length check
 
 fs.readFile(
-		
-	__dirname + '/' + infilename, // Input file path
-	
+
+	infilename, // Input file path
+
 	function( err, bytes ){
 		if(err){ console.log(err); throw err; }
-	
+
 		var reader = new PNGReader(bytes);
-	
+
 		reader.parse( function( err, png ){
 			if(err){ console.log(err); throw err; }
-			
+
 			// creating an ImageData object
 			var myImageData = { width:png.width, height:png.height, data:png.pixels };
-			
+
 			// tracing to SVG string
 			var svgstring = ImageTracer.imagedataToSVG( myImageData, options );
-			
+
 			// writing to file
 			fs.writeFile(
-				__dirname + '/' + outfilename, // Output file path
+				outfilename, // Output file path
 				svgstring,
 				function(err){ if(err){ console.log(err); throw err; } console.log( __dirname + '/'+outfilename+' was saved!' ); }
 			);
-			
+
 		});// End of reader.parse()
-		
+
 	}// End of readFile callback()
-	
+
 );// End of fs.readFile()
