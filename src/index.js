fdcef1942d2edb1672bcc5790978b8a41a1dc5c4
Lance Whatley <whatl3y@gmail.com>
2020-06-21 15:51:30 -0400

    bind PNGReader on itself

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const imagetracerjs = require ('./imagetracer_v1.2.6')
 const PNGReader = require('./PNGReader')
 
 module.exports = imagetracerjs
-module.exports.PNGReader = PNGReader
+module.exports.PNGReader = PNGReader.bind(PNGReader)
